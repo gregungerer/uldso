86c5999aae99fe22f37102bef6d9849880114306
Greg Ungerer <gerg@kernel.org>
2023-02-16 23:48:59 +1000

    uldso: fix ARM relocation processing
    Unlike M68K and RISCV architecture ELF format binaries ARM platforms use
    the ELF "rel" section and not the "rela" section for their relocations.
    It is similar but not identical and needs to be processed slightly
    differently.
    
    For the most part the procedure followed is the same, just the section
    identifiers and relocation data structure is different. With a bit of
    C-preprocessor abstracting the code is mostly the same.
    
    Signed-off-by: Greg Ungerer <gerg@kernel.org>

--- linker.c
+++ linker.c
@@ -15,6 +15,7 @@
 #define elf_hdr			elf64_hdr
 #define elf_phdr		elf64_phdr
 #define elf_shdr		elf64_shdr
+#define elf_rel			elf64_rel
 #define elf_rela		elf64_rela
 #define elf_dynamic		Elf64_Dyn
 #define Elf_Addr		Elf64_Addr
@@ -27,6 +28,7 @@
 #define elf_hdr			elf32_hdr
 #define elf_phdr		elf32_phdr
 #define elf_shdr		elf32_shdr
+#define elf_rel			elf32_rel
 #define elf_rela		elf32_rela
 #define elf_dynamic		Elf32_Dyn
 #define Elf_Addr		Elf32_Addr
@@ -47,27 +49,58 @@
  * define these numbers (well, at least for some architectures).
  */
 #ifdef arm
+#define RELOCATE
 #define R_RELATIVE	23 /*R_ARM_RELATIVE*/
 #endif
 
 #ifdef m68k
+#define RELOCATEA
 #define R_RELATIVE	22 /*R_68K_RELATIVE*/
 #endif
 
 #ifdef riscv
+#define RELOCATEA
 #define R_RELATIVE	3 /*R_RISCV_RELATIVE*/
 #endif
 
-static void relocate(unsigned long loadaddr, struct elf_rela *rela)
+/*
+ * Abstract away the differences in dealing with "rel" sections or "rela"
+ * sections. The relocation calculation is slightly different, not to
+ * mention that the data structures involved are ever so similar but
+ * still slightly different. In practice each architecture supports one
+ * or the other - but not both.
+ */
+#ifdef RELOCATE
+#define DT_RELOCATION	DT_REL
+#define DT_RELOCATIONSZ	DT_RELSZ
+#define elf_relocation	elf_rel
+static inline void relative(unsigned long loadaddr, struct elf_rel *rel)
+{
+	unsigned long *paddr;
+	paddr = (unsigned long *) (loadaddr + rel->r_offset);
+	*paddr += loadaddr;
+}
+#else
+#define DT_RELOCATION	DT_RELA
+#define DT_RELOCATIONSZ	DT_RELASZ
+#define elf_relocation	elf_rela
+static inline void relative(unsigned long loadaddr, struct elf_rela *rela)
+{
+	unsigned long *paddr;
+	if (rela->r_addend) {
+		paddr = (unsigned long *) (loadaddr + rela->r_offset);
+		*paddr = loadaddr + rela->r_addend;
+	}
+}
+#endif
+
+static void relocate(unsigned long loadaddr, struct elf_relocation *rel)
 {
 	unsigned long *paddr;
 
-	switch (rela->r_info) {
+	switch (rel->r_info) {
 	case R_RELATIVE:
-		if (rela->r_addend) {
-			paddr = (unsigned long *) (loadaddr + rela->r_offset);
-			*paddr = loadaddr + rela->r_addend;
-		}
+		relative(loadaddr, rel);
 		break;
 	}
 }
@@ -75,27 +108,27 @@ static void relocate(unsigned long loadaddr, struct elf_rela *rela)
 unsigned long linker(struct elf_fdpic_loadmap *map, elf_dynamic *dynp)
 {
 	struct elf_fdpic_loadseg *seg;
-	struct elf_rela *rela;
+	struct elf_relocation *rel;
 	struct elf_hdr *elfp;
 	unsigned long loadaddr;
-	int i, rela_cnt;
+	int i, size;
 
 	seg = map->segs;
 	loadaddr = seg->addr;
 
 	/* Find the .rela.dyn section start and size */
-	for (i = 0; i < 32; i++, dynp++) {
-		if (dynp->d_tag == DT_RELA)
-			rela = (struct elf_rela *) (loadaddr + dynp->d_un.d_ptr);
-		if (dynp->d_tag == DT_RELASZ)
-			rela_cnt = dynp->d_un.d_val / sizeof(struct elf_rela);
+	for (i = 0; i < 64; i++, dynp++) {
+		if (dynp->d_tag == DT_RELOCATION)
+			rel = (struct elf_relocation *) (loadaddr + dynp->d_un.d_ptr);
+		if (dynp->d_tag == DT_RELOCATIONSZ)
+			size = dynp->d_un.d_val / sizeof(struct elf_relocation);
 		if (dynp->d_tag == DT_NULL)
 			break;
 	}
 
 	/* Process the relocations */
-	for (i = 0; i < rela_cnt; i++, rela++)
-		relocate(loadaddr, rela);
+	for (i = 0; i < size; i++, rel++)
+		relocate(loadaddr, rel);
 
 	elfp = (struct elf_hdr *) loadaddr;
 	return loadaddr + elfp->e_entry;
